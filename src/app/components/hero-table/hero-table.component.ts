8555da063b7280d072d1e0fd3afb54f205d13991
frosty <aaronfrost@gmail.com>
2020-03-31 17:00:30 -0600

    Final

--- src/app/components/hero-table/hero-table.component.ts
+++ src/app/components/hero-table/hero-table.component.ts
@@ -1,50 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map, withLatestFrom } from 'rxjs/operators';
 import { Hero, HeroService } from '../../services/hero.service';
+
+interface HeroTableVM {
+    heroes: Hero[];
+    search: string;
+    page: number;
+    loading: boolean;
+    totalHeroes: number;
+    totalPages: number;
+    limit: number;
+}
 
 @Component({
     selector: 'rx-hero-table',
     template: `
-        <div class="tool-bar">
-            <span class="search-tool">
-                <label for="herosearch">Search: </label>
-                <input name="herosearch" />
-            </span>
-            <span class="page-tool">
-                <label>Page ? of ? : </label>
-                <span class="buttons">
-                    <button class="prev">Prev</button>
-                    <button class="next">Next</button>
+        <ng-container *ngIf="vm$ | async as vm">
+            <h5 *ngIf="vm.loading">Loading</h5>
+            <div class="tool-bar">
+                <span class="search-tool">
+                    <label for="herosearch">Search: </label>
+                    <input name="herosearch" [value]="vm.search" (input)="hero.setSearch($event.target.value)" />
                 </span>
-            </span>
-            <span class="result-tool">
-                <label>Show Results: </label>
-                <span class="buttons">
-                    <button *ngFor="let limit of hero.limits">
-                        {{ limit }}
-                    </button>
+                <span class="page-tool">
+                    <label>Page {{ vm.page }} of {{ vm.totalPages }} : </label>
+                    <span class="buttons">
+                        <button class="prev" [disabled]="vm.page <= 1" (click)="hero.movePageBy(-1)">Prev</button>
+                        <button class="next" [disabled]="vm.page == vm.totalPages" (click)="hero.movePageBy(1)">
+                            Next
+                        </button>
+                    </span>
                 </span>
-            </span>
-            <span class="total-tool">
-                <label>Total Results: ???</label>
-            </span>
-        </div>
-        <div class="table-content">
-            <rx-hero-badge
-                *ngFor="let hero of heroes"
-                [hero]="hero"
-            ></rx-hero-badge>
-        </div>
+                <span class="result-tool">
+                    <label>Show Results: </label>
+                    <span class="buttons">
+                        <button
+                            *ngFor="let limit of hero.limits"
+                            [disabled]="limit === vm.limit"
+                            (click)="hero.setLimit(limit)"
+                        >
+                            {{ limit }}
+                        </button>
+                    </span>
+                </span>
+                <span class="total-tool">
+                    <label>Total Results: {{ vm.totalHeroes }}</label>
+                </span>
+            </div>
+            <div class="table-content">
+                <rx-hero-badge *ngFor="let hero of vm.heroes" [hero]="hero"></rx-hero-badge>
+            </div>
+        </ng-container>
     `,
     styleUrls: ['./hero-table.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeroTableComponent implements OnInit {
-    heroes: Hero[];
+export class HeroTableComponent {
+    public vm$: Observable<HeroTableVM> = combineLatest([
+        this.hero.totalPages$,
+        this.hero.totalHeroes$,
+        this.hero.heroes$,
+        this.hero.loading$,
+    ]).pipe(
+        withLatestFrom(this.hero.search$, this.hero.page$, this.hero.limit$),
+        map(([[totalPages, totalHeroes, heroes, loading], search, page, limit]) => ({
+            heroes,
+            search,
+            page: page + 1,
+            loading,
+            totalHeroes,
+            totalPages,
+            limit,
+        })),
+    );
 
-    constructor(public hero: HeroService) {
-        hero.heroes$.subscribe(heroes => {
-            this.heroes = heroes;
-        });
-    }
-
-    ngOnInit() {}
+    constructor(public hero: HeroService) {}
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,8 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import {
+    debounceTime,
+    distinctUntilChanged,
+    map,
+    pluck,
+    shareReplay,
+    switchMap,
+    tap,
+    withLatestFrom,
+} from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+
+interface HeroResponse {
+    data: {
+        total: number;
+        results: Hero[];
+    };
+}
 
 export interface Hero {
     id: number;
@@ -42,22 +58,104 @@ const LIMIT_MID = 25;
 const LIMIT_HIGH = 100;
 const LIMITS = [LIMIT_LOW, LIMIT_MID, LIMIT_HIGH];
 
+interface HeroTableState {
+    search: string;
+    page: number;
+    limit: number;
+    loading: boolean;
+}
+
+const initialState: HeroTableState = {
+    search: '',
+    page: 0,
+    limit: LIMIT_LOW,
+    loading: false,
+};
+
 @Injectable({
     providedIn: 'root',
 })
 export class HeroService {
     limits = LIMITS;
 
-    heroes$: Observable<Hero[]> = this.http
-        .get(HERO_API, {
-            params: {
+    private heroState$: BehaviorSubject<HeroTableState> = new BehaviorSubject(initialState);
+
+    search$ = this.heroState$.pipe(
+        pluck('search'),
+        distinctUntilChanged(),
+    );
+    page$ = this.heroState$.pipe(
+        pluck('page'),
+        distinctUntilChanged(),
+    );
+    limit$ = this.heroState$.pipe(
+        pluck('limit'),
+        distinctUntilChanged(),
+    );
+    loading$ = this.heroState$.pipe(
+        pluck('loading'),
+        distinctUntilChanged(),
+    );
+
+    private params$ = combineLatest([this.search$, this.page$, this.limit$]);
+
+    private heroesResponse$: Observable<HeroResponse> = this.params$.pipe(
+        debounceTime(500),
+        tap(() => this.toggleLoading(true)),
+        switchMap(([search, page, limit]) => {
+            const params: any = {
                 apikey: environment.MARVEL_API.PUBLIC_KEY,
-                limit: `${LIMIT_LOW}`,
-                // nameStartsWith: 'iron', // once we have search
-                offset: `${0}`, // page * limit
-            },
-        })
-        .pipe(map((res: any) => res.data.results));
+                limit: `${limit}`,
+                offset: `${page * limit}`, // page * limit
+            };
+            if (search && search.length) {
+                params.nameStartsWith = search;
+            }
+            return this.http.get<HeroResponse>(HERO_API, { params });
+        }),
+        tap(() => this.toggleLoading(false)),
+        shareReplay(1),
+    );
+
+    heroes$ = this.heroesResponse$.pipe(pluck('data', 'results'));
+    totalHeroes$ = this.heroesResponse$.pipe(pluck('data', 'total'));
+    totalPages$ = this.totalHeroes$.pipe(
+        withLatestFrom(this.limit$),
+        map(([total, limit]) => Math.ceil(total / limit)),
+    );
 
     constructor(private http: HttpClient) {}
+
+    private toggleLoading(loading: boolean) {
+        const state = this.heroState$.getValue();
+        this.heroState$.next({
+            ...state,
+            loading: loading,
+        });
+    }
+
+    setLimit(limit: number) {
+        const state = this.heroState$.getValue();
+        this.heroState$.next({
+            ...state,
+            page: 0,
+            limit,
+        });
+    }
+    setSearch(search: string) {
+        const state = this.heroState$.getValue();
+        this.heroState$.next({
+            ...state,
+            page: 0,
+            search,
+        });
+    }
+
+    movePageBy(moveBy: number) {
+        const state = this.heroState$.getValue();
+        this.heroState$.next({
+            ...state,
+            page: state.page + moveBy,
+        });
+    }
 }
