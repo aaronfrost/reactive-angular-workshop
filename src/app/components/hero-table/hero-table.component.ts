e0ccd9d46a4e72fb101908bbe2c7c1d98f34533a
frosty <aaronfrost@gmail.com>
2020-11-17 13:52:01 -0700

    Switch to use a VM.

--- src/app/components/hero-table/hero-table.component.ts
+++ src/app/components/hero-table/hero-table.component.ts
@@ -9,11 +9,42 @@ import { Hero, HeroService } from '../../services/hero.service';
     styleUrls: ['./hero-table.component.scss'],
 })
 export class HeroTableComponent {
-    heroes: Hero[];
-
     disablePrevious$ = this.hero.page$.pipe(map(page => page <= 0));
     disableNext$ = combineLatest([this.hero.totalPages$, this.hero.page$]).pipe(
         map(([totalPages, page]) => page >= totalPages - 1),
+    );
+
+    vm$ = combineLatest([
+        this.hero.heroes$,
+        this.hero.search$,
+        this.hero.userPage$,
+        this.hero.totalPages$,
+        this.disablePrevious$,
+        this.disableNext$,
+        this.hero.limit$,
+        this.hero.total$,
+    ]).pipe(
+        map(
+            ([
+                heroes,
+                search,
+                userPage,
+                totalPages,
+                disablePrevious,
+                disableNext,
+                limit,
+                total,
+            ]) => ({
+                heroes,
+                search,
+                userPage,
+                totalPages,
+                disablePrevious,
+                disableNext,
+                limit,
+                total,
+            }),
+        ),
     );
 
     constructor(public hero: HeroService) {}
