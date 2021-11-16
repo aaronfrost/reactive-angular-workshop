e56f277128d3d61cc4937f712501ebaa9a127ef4
frosty <aaronfrost@gmail.com>
2021-11-15 23:26:42 -0700

    Upgrade to v13

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,12 +15,10 @@ if (environment.MARVEL_API.PUBLIC_KEY === 'INSERT YOUR KEY FIRST') {
      * Once you have done that, in their portal, you will need to add http://localhost to their
      * whitelisted domains. If you don't do this, it will fail for you.
      */
-    setTimeout(() => {
-        document.write(`You need to insert your Marvel API key before you begin. <BR>
+    document.body.innerHTML = `You need to insert your Marvel API key before you begin. <BR>
             Open your <code>environments.ts</code> file and add your Marvel keys on line 4 and 5<BR><BR>
             If you are seeing this, and don't understand why, please let the Frosty know<BR>
-            and he will help you out.`);
-    })
+            and he will help you out.`;
 
     throw new Error('You must setup a public and private API key first.');
 }
